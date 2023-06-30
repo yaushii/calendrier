2f4479613c62870c91f11eb9eb0f21186e8d487e
justine <justine.telmann@gmail.com>
2023-06-30 14:34:39 +0200

    first commit

---
+++ php/index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/calendar.css">
+</head>
+
+<body>
+
+    <nav class="navbar navbar-dark bg-primary mb-3">
+        <a href="/index.php" class="navbar-brand">Calendrier Apprenti</a>
+    </nav>
+
+    <?php
+    require '../src/date/month.php';
+    /* <?php try {
+
+        $month = new App\Date\Month($_GET['month'] ?? null, $_GET['year'] ?? null);
+    } catch (\Exception $e) {
+        $month = new App\Date\Month();
+    } ?>*/
+
+    $month = new App\Date\Month($_GET['month'] ?? null, $_GET['year'] ?? null); ?>
+
+    <h1><?= $month->toString(); ?></h1>
+
+    <?= $month->getWeeks(); ?>
+
+    <table class ="calendarTable">
+        <?php
+        for ($i = 0; $i < $month->getWeeks(); $i++) { ?>
+            <tr>
+                <td>Lundi</td>
+                <td>Mardi</td>
+                <td>Mercredi</td>
+                <td>Jeudi</td>
+                <td>Vendredi</td>
+                <td>Samedi</td>
+                <td>Dimanche</td>
+            </tr>
+        <?php } ?>
+
+
+
+
+
+    </table>
+
+
+
+</body>
+
+</html>
